f1cc5e69ca21bf08ffb946be8017a7293e3aa858
Sam <sam@switchlat>
2019-08-30 09:21:49 +0100

    get-telemetry-gen3: Add start/end datetimes to API req

--- get-telemetry-gen3.py
+++ get-telemetry-gen3.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+# Expect an API request to be like so:
+#   GET <base-url>/switchee/gen3/telemetry/<client_id>/<start>/<end>
+# where start and end time formats are YYMMDD_hhmmss, e.g:
+#   19/08/27 13:14:24 -> 190827_131424
+
 import json
 
 PREFIX = "/switchee/gen3/telemetry"
@@ -12,8 +17,14 @@ def make_body(event):
     return body
   if path.startswith(PREFIX):
     rest = path[1+len(PREFIX):] # get string after PREFIX (and /)
-    client_id = rest.split("/")[0] # split on / and get first entry  
-    body = client_id # this is the client id
+    fields = rest.split("/")
+    try:
+      client_id = fields[0]
+      start = fields[1]
+      end = fields[2]
+      body = "GET msgs for {0} from {1} to {2}".format(client_id, start, end)
+    except IndexError:
+      pass
   return body
 
 def lambda_handler(event, context):
